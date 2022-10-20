3eac70a120a78ac3557fe5048d36c65ea2084625
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-19 22:51:31 -0300

    first commit

---
+++ Task.Api/Data/DbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Task.Api.Data
+{
+    public class DbContext
+    {
+    }
+}
---
+++ Task.Api/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task.Api.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Owner { get; set; }
+  
+    }
+}
